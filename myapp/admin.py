de07d532fc4ceca948b7741770db569781030835
Alex Ponomarev <63734825+Alpono24@users.noreply.github.com>
2025-10-26 22:24:35 +0300

    Delete myapp directory

--- myapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-# from myproject.myapp.models import Product
-from .models import Post
-
-
-@admin.register(Post)
-class ObjectAdmin(admin.ModelAdmin):
-    list_display = ('title', 'body', 'author')
